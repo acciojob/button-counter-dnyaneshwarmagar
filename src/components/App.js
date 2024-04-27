5544ee63ea0350d456a064807a63a381320ab193
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-04-28 02:17:17 +0530

    d

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
+import React, { useState } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+const ButtonCounter = () => {
+  // Initialize the counter state to 0
+  const [count, setCount] = useState(0);
 
-const App = () => {
+  // Event handler to increment the counter when the button is clicked
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Display the count of button clicks */}
+      <p>Button clicked {count} times</p>
+      {/* Button to increment the counter */}
+      <button onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default ButtonCounter;
